744f7166333a93cf4dab7f570324f3715949c167
Jonas Schell <jonasschell@me.com>
2015-05-25 01:48:03 -0400

    Added threaded save frame function with timer to save frames automatically; Added ellipse drawing instead of modifying the pixels directly to get soother lines;

--- src/agentpicmerge/AgentPicMerge.java
+++ src/agentpicmerge/AgentPicMerge.java
@@ -1,10 +1,12 @@
 package agentpicmerge;
 
+import java.sql.Time;
+
+import com.sun.tools.javac.util.Convert;
+
 import processing.core.PApplet;
 import processing.core.PImage;
-import processing.core.PVector;
 // to find the right pixel pixelarrayNr = x+(y*width);
-import toxi.geom.Vec2D;
 import punktiert.math.*;
 import punktiert.physics.VBoid;
 import punktiert.physics.VParticle;
@@ -18,14 +20,27 @@ public class AgentPicMerge extends PApplet {
 	PImage canvas, pic1, pic2;
 	
 	VPhysics physics;
-	VParticleGroup groupe01, gourpe02;
+	VParticleGroup groupe01, groupe02;
 	
 	boolean safeFrame = false;
+	boolean drawAgents = false;
+	
+	float saveIntervallTimer;
+	float waitToSave;
+	float lastTime;
+	String uniqueFolderToSaveTo;
 	
 	public void setup() 
 	{
 		size(500,500);
 		frameRate(2000);
+		smooth();
+
+		saveIntervallTimer = millis();
+		waitToSave = 4000;
+		background(0);
+		uniqueFolderToSaveTo =str(day())+"_"+str(month())+"_"+str(year())+"-"+str(hour())+"_"+str(minute())+"_"+str(second());
+		println("Folder to save to" + uniqueFolderToSaveTo+ " in "+waitToSave+" mil s intervalls.");
 
 		canvas = createImage(500, 500, RGB);
 		pic1 = loadImage("pic01.jpg");
@@ -37,58 +52,86 @@ public class AgentPicMerge extends PApplet {
 		
 		//punktiert code
 		physics = new VPhysics(width, height);
+		groupe01 = new VParticleGroup();
+		groupe02 = new VParticleGroup();
 		
-		int amount = 10;
+		int amount = 30;
 
 		for (int i = 0; i < amount; i++) 
 		{
 			Vec pos = new Vec(random(10, width), random(10, height));
-			float rad = random(3, 6);
+			float rad = 5;
 			VBoid p = new VBoid(pos);
-			p.swarm.setSeperationScale((float) (rad*.7));
+			p.swarm.setSeperationScale((float) (rad));
 			p.setRadius(rad);
 			physics.addParticle(p);
-//			if(i < amount/2){
-//				groupe01.addParticle(p);
-//			}else{
-//				gourpe02.addParticle(p);
-//			}
-//			
-//			physics.addGroup(groupe01);
-//			physics.addGroup(gourpe02);
+			//add particles to two diffrent groupes
+			if(i < amount/2){
+				groupe01.addParticle(p);
+			}else{
+				groupe02.addParticle(p);
+			}
+			physics.addGroup(groupe01);
+			physics.addGroup(groupe02);
 		}
-		  
-		  
 	}
 
 	public void draw() 
 	{
-		background(0);
+		saveIntervallTimer += millis() - lastTime;
+		
+//		background(0);
 		physics.update();
 		
 		//canvas img is going to be manipulated here
-		canvas.loadPixels();
-		for (VParticle p : physics.particles) 
+//		canvas.loadPixels();
+		
+		noStroke();
+		for(VParticleGroup g : physics.groups)
 		{
-			int pixLoc1 = getPixLoc(round(p.x), round(p.y));
-			canvas.pixels[pixLoc1] = color(pic1.pixels[pixLoc1]);
+			for (VParticle p : g.particles) 
+			{
+				float r = .5f; //ellipse radius
+				int pixLoc = getPixLoc(round(p.x), round(p.y));
+				if(g == groupe01){
+//						canvas.pixels[pixLoc] = color(pic1.pixels[pixLoc]);
+					fill(color(pic1.pixels[pixLoc]));
+					ellipse(p.x, p.y, r, r);
+				}else{
+//						canvas.pixels[pixLoc] = color(pic2.pixels[pixLoc]);
+					fill(color(pic2.pixels[pixLoc]));
+					ellipse(p.x, p.y, r, r);
+				}
+			}
 		}
-		canvas.updatePixels();
-		image(canvas, 0, 0);
-
-		for (VParticle p : physics.particles)
+		
+//		canvas.updatePixels();
+//		image(canvas, 0, 0);
+		
+		//draw agents as ellipses if 'd' was pressed
+		if(drawAgents)
 		{
-			ellipse(p.x, p.y, p.getRadius()*2, p.getRadius()*2);
+			for (VParticle p : physics.particles)
+			{
+				ellipse(p.x, p.y, p.getRadius()*2, p.getRadius()*2);
+			}
 		}
 		
 		//if key 's' is pressed take a screenshot
-		if (safeFrame){
-			saveFrame("test_#####.png");
-			safeFrame = false;
+		if (safeFrame && saveIntervallTimer > waitToSave)
+		{
+			thread("saveFrameToDisk");
+			saveIntervallTimer = 0;
 		}
-		println(frameRate);
+//		println(frameRate);
+		lastTime = millis();
 	}
 	
+	public void saveFrameToDisk()
+	{
+		saveFrame("Render/"+uniqueFolderToSaveTo + "/test_#####.png");
+		println("frameSafed");
+	}
 	
 	int getPixLoc( float x_, float y_ ){
 		
@@ -104,11 +147,14 @@ public class AgentPicMerge extends PApplet {
 	
 	public void keyPressed(){
 		if(key == 'r') setup();
+		if(key == 'd') drawAgents = !drawAgents;
+		if(key == 's') safeFrame = !safeFrame;
 	}
 	
 	public void keyDown(){
-		if(key == 's') safeFrame = !safeFrame;
+		
 	}
+	
 	
 	public static void main(String _args[]) {
 		PApplet.main(new String[] {  agentpicmerge.AgentPicMerge.class.getName() });
